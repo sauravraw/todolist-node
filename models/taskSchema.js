cf1f845f2e1765b9edd6d97081c3d191adfd4033
Saurav <saurav.upadhyay@raweng.com>
2021-09-16 16:54:33 +0530

    validation updated

--- models/taskSchema.js
+++ models/taskSchema.js
@@ -2,46 +2,45 @@ const mongoose = require("mongoose");
 const uniqid = require("uniqid");
 
 const taskSchema = new mongoose.Schema({
-	taskId: {
-		type: String,
-		default: uniqid(),
-		unique: true,
-	},
-	taskName: {
-		type: String,
-		required: [true, "Please input task"], // required in in-build validator
-		validate: {
-			validator: function () {
-				// console.log("this is a task validator");
-				return this.taskName.trim().length;
-			},
-			message: "Task name should not be empty",
-		},
-		validate: {
-			validator: function () {
-				let re = /<("[^"]?"|'[^']?'|[^'">])*>/;
-				if (re.test(this.taskName)) {
-					return false;
-				}
-			},
-			message: "Task content cannot be HTML.",
-		},
-	},
-	status: {
-		type: String,
-		default: "notStarted",
-		enum: ["notStarted", "inProgress", "completed"],
-	},
-	createdAt: {
-		type: Date,
-		default: Date.now(),
-	},
-	startedAt: {
-		type: Date,
-	},
-	completedAt: {
-		type: Date,
-	},
+  taskId: {
+    type: String,
+    default: uniqid(),
+    unique: true,
+  },
+  taskName: {
+    type: String,
+    required: [true, "Please input task"], // required in in-build validator
+    validate: {
+      validator: function () {
+        return this.taskName.trim().length;
+      },
+      message: "Task name should not be empty",
+    },
+    validate: {
+      validator: function () {
+        const re = /<("[^"]*?"|'[^']*?'|[^'">])*>/;
+        if (re.test(this.taskName)) {
+          return false;
+        }
+      },
+      message: "Task content cannot be HTML",
+    },
+  },
+  status: {
+    type: String,
+    default: "notStarted",
+    enum: ["notStarted", "inProgress", "completed"],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startedAt: {
+    type: Date,
+  },
+  completedAt: {
+    type: Date,
+  },
 });
 
 const Task = mongoose.model("Task", taskSchema);
